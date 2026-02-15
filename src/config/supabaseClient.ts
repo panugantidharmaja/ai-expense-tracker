576e2d62f4b5220ef5e8dd86d17c82dd99c02008
Dharma <panugantidharmaja@gmail.com>
2026-02-14 22:31:36 -0600

    remove debug logs

--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -2,9 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-console.log('URL:', supabaseUrl);
-console.log('KEY:', supabaseAnonKey);
-
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
